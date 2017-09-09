0b24ecc824e28ecefa6d0c5b29c84175ae78d832
AleAlvs <aleson.a.s@gmail.com>
2017-09-09 16:51:37 -0300

    funcionalidade nova - random date

---
+++ app/src/main/java/com/aleson/example/mangapp/RandomDate.java
@@ -0,0 +1,69 @@
+package com.aleson.example.mangapp;
+
+import java.util.Random;
+
+/**
+ * Created by GAMER on 09/09/2017.
+ */
+
+public class RandomDate {
+
+    public String getRandomDate() {
+        return randomDate;
+    }
+
+    public void setRandomDate(String randomDate) {
+        this.randomDate = randomDate;
+    }
+
+    private String randomDate;
+    private int[] monthRange = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private int todayDay;
+    private int todayMonth;
+    private int todayYear;
+
+
+    public RandomDate(String date){
+        this.todayYear = Integer.parseInt(date.substring(0,4));
+        this.todayMonth = Integer.parseInt(date.substring(6,7));
+        this.todayDay = Integer.parseInt(date.substring(9,10));
+        generate();
+    }
+
+    private void generate(){
+        Random year = new Random();
+        Random month = new Random();
+        Random day = new Random();
+        int yearR = 1995 + year.nextInt((todayYear - 1995) + 1);
+        int monthR = 1 + month.nextInt((12 - 1)+1);
+        int dayR = 1 + day.nextInt((31 - 1) + 1);
+        if(monthR == 2 && dayR >= 28) {
+            this.generate();
+        }
+        if(yearR == 1995 && monthR < 6){
+            this.generate();
+        }
+        if(yearR == 1995 && monthR == 6 && dayR < 16){
+            this.generate();
+        }
+        if(todayYear == yearR && monthR > todayMonth){
+            this.generate();
+        }
+        if(todayYear == yearR && todayMonth == monthR && todayDay > dayR){
+            this.generate();
+        }
+        if(!validateMonth(monthR, dayR)){
+            this.generate();
+        }
+        setRandomDate(yearR+"-"+String.format("%01d", monthR)+"-"+String.format("%01d", dayR));
+    }
+
+    private boolean validateMonth(int monthR, int dayR){
+        if(dayR <= monthRange[--monthR]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
