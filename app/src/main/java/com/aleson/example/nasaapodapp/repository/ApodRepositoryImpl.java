d30292c8a7717843ba998b0a4b44f3574488b7ef
AleAlvs <aleson.a.s@gmail.com>
2017-09-11 23:50:07 -0300

    tratamento de erro

--- app/src/main/java/com/aleson/example/nasaapodapp/repository/ApodRepositoryImpl.java
+++ app/src/main/java/com/aleson/example/nasaapodapp/repository/ApodRepositoryImpl.java
@@ -12,7 +12,7 @@ public class ApodRepositoryImpl implements ApodRepository {
     private Activity mActivity;
     private MainActivityView mainActivityView;
 
-    public ApodRepositoryImpl(Activity mActivity){
+    public ApodRepositoryImpl(Activity mActivity) {
         this.mActivity = mActivity;
         mainActivityView = (MainActivityView) mActivity;
     }
@@ -29,11 +29,12 @@ public class ApodRepositoryImpl implements ApodRepository {
         Gson gson = new Gson();
         ApodModel model = gson.fromJson(response, ApodModel.class);
         mainActivityView.onFinishLoad();
-        if(model.getUrl().contains(".gif")){
+        if (model.getUrl() == null) {
+            mainActivityView.onError("Erro");
+        } else if (model.getUrl().contains(".gif")) {
             mainActivityView.loadGif(model);
-        }
-        else {
-            if(model.getUrl().contains(".jpg") || model.getUrl().contains(".jpeg") || model.getUrl().contains(".png")){
+        } else {
+            if (model.getUrl().contains(".jpg") || model.getUrl().contains(".jpeg") || model.getUrl().contains(".png")) {
                 mainActivityView.loadImage(model);
             }
             else{
