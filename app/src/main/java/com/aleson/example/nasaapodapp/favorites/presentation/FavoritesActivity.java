615c547bf69b3f499479de6799074a74b96eae13
Aleson Alves de Souza <aleson.a.s@gmail.com>
2017-12-23 20:38:17 -0200

    correcao de favoritos

--- app/src/main/java/com/aleson/example/nasaapodapp/favorites/presentation/FavoritesActivity.java
+++ app/src/main/java/com/aleson/example/nasaapodapp/favorites/presentation/FavoritesActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
 import android.view.Display;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -39,7 +41,7 @@ public class FavoritesActivity extends AppCompatActivity implements FavoritesVie
     private TextView textViewNofavorites;
     private ProgressBar progressBarLoading;
     private FavoritesPresenter favoritesPresenter;
-    private RelativeLayout relativeLayoutLoading;
+    private LinearLayout relativeLayoutLoading;
     RecyclerView recyclerView;
     RecyclerView.Adapter recyclerViewAdapter;
     RecyclerView.LayoutManager recylerViewLayoutManager;
@@ -50,9 +52,9 @@ public class FavoritesActivity extends AppCompatActivity implements FavoritesVie
         mActivity = this;
         context = this;
         setContentView(R.layout.activity_favorites);
-        textViewNofavorites = (TextView) findViewById(R.id.textview_no_service);
+        textViewNofavorites = (TextView) findViewById(R.id.textview_no_favorites);
         progressBarLoading = (ProgressBar) findViewById(R.id.progressbar_loading_image);
-        relativeLayoutLoading = (RelativeLayout) findViewById(R.id.loading_image);
+        relativeLayoutLoading = (LinearLayout) findViewById(R.id.loading_image);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setTitle("Favorites");
         setSupportActionBar(myToolbar);
@@ -90,8 +92,14 @@ public class FavoritesActivity extends AppCompatActivity implements FavoritesVie
 
     private String getDeviceImei() {
         TelephonyManager mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        String deviceid = mTelephonyManager.getDeviceId();
-        return deviceid;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            String deviceid = mTelephonyManager.getDeviceId();
+            return deviceid;
+        }
+        else{
+            String deviceid = mTelephonyManager.getDeviceId();
+            return deviceid;
+        }
     }
 
     private void adapter(ArrayList<Apod> model) {
@@ -104,8 +112,9 @@ public class FavoritesActivity extends AppCompatActivity implements FavoritesVie
             textViewNofavorites.setVisibility(View.VISIBLE);
             textViewNofavorites.setText("Nothing here yet");
             progressBarLoading.setVisibility(View.GONE);
+            relativeLayoutLoading.setVisibility(View.VISIBLE);
         } else {
-            relativeLayoutLoading.setGravity(View.GONE);
+            relativeLayoutLoading.setVisibility(View.GONE);
             textViewNofavorites.setVisibility(View.GONE);
             progressBarLoading.setVisibility(View.GONE);
         }
