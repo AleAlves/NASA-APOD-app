3c27949bb67e64c0938878b0b5c63a15f0413a21
Aleson Alves <asouza@isban.com.br>
2019-03-16 09:10:25 -0300

    Removing all files

--- app/src/main/java/com/aleson/example/nasaapodapp/about/presenter/AboutPresenter.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018 aleson.a.s@gmail.com, All Rights Reserved.
-
-package com.aleson.example.nasaapodapp.about.presenter;
-
-public interface AboutPresenter {
-    void getServiceVersion();
-    void setServiceVersion(String version);
-}
